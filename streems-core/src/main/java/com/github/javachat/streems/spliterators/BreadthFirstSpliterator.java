7af19d25bc1c9524390a0df1f856edaa420df476
Francis Galiegue <fgaliegue@gmail.com>
2016-04-28 19:53:14 +0200

    BreadthFirstSpliterator: change the Deque to accept iterators instead of elements

--- streems-core/src/main/java/com/github/javachat/streems/spliterators/BreadthFirstSpliterator.java
+++ streems-core/src/main/java/com/github/javachat/streems/spliterators/BreadthFirstSpliterator.java
@@ -1,6 +1,7 @@
 package com.github.javachat.streems.spliterators;
 
 import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.function.Consumer;
@@ -14,22 +15,33 @@ import java.util.function.Function;
 public final class BreadthFirstSpliterator<T>
     extends NaryTreeSpliterator<T>
 {
-    private final Deque<T> deque = new ArrayDeque<>();
+    private final Deque<Iterator<T>> deque = new ArrayDeque<>();
 
     public BreadthFirstSpliterator(final T root,
         final Function<T, Iterator<T>> fn)
     {
         super(root, fn);
-        deque.addFirst(root);
+        deque.addFirst(Collections.singleton(root).iterator());
     }
 
+    @SuppressWarnings("TailRecursion")
     @Override
     public boolean tryAdvance(final Consumer<? super T> action)
     {
+        final Iterator<T> iterator;
+        final T element;
+
         if (deque.isEmpty())
             return false;
-        final T element = deque.removeFirst();
-        fn.apply(element).forEachRemaining(deque::addLast);
+
+        iterator = deque.getFirst();
+        if (!iterator.hasNext()) {
+            deque.removeFirst();
+            return tryAdvance(action);
+        }
+
+        element = iterator.next();
+        deque.add(fn.apply(element));
         action.accept(element);
         return true;
     }
